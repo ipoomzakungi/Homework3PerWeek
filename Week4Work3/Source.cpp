3ca61c09fabec575de04ee7400ce8acbe031b5c4
Punnawat <poombotkub3@hotmail.com>
2020-09-06 12:35:11 +0700

    Right Triangle

--- Week4Work3/Source.cpp
+++ Week4Work3/Source.cpp
@@ -4,9 +4,55 @@
 
 int main()
 {
-	double a, b, c;
+	double a, b, c,max,min,mid;
 	scanf("%lf %lf %lf", &a, &b, &c);
-	if (pow(a, 2) + pow(b, 2) == pow(c, 2))
+	if (a > b && a > c)
+	{
+		if (b >= c)
+		{
+			max = a;
+			min = c;
+			mid = b;
+		}
+		else
+		{
+			max = a;
+			min = b;
+			mid = c;
+		}
+	}
+	if (b > a && b > c)
+	{
+		if (a >= c)
+		{
+			max = b;
+			min = c;
+			mid = a;
+		}
+		else
+		{
+			max = b;
+			min = a;
+			mid = c;
+		}
+	}
+	if (c > b && c > a)
+	{
+		if (a >= b)
+		{
+			max = c;
+			min = b;
+			mid = a;
+		}
+		else
+		{
+			max = c;
+			min = a;
+			mid = b;
+		}
+	}
+	//printf("%.2lf %.2lf %.2lf", max, min, mid);
+	if (pow(min, 2) + pow(mid, 2) == pow(max, 2))
 	{
 		printf("right triangle");
 	}
