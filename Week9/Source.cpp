2dfdd175505256879579a18deb40420df6bfc77a
Punnawat <poombotkub3@hotmail.com>
2020-10-11 20:16:02 +0700

    WORD CHAIN !

--- Week9/Source.cpp
+++ Week9/Source.cpp
@@ -3,8 +3,9 @@
 int main() {
 
 	int len=0, N=0,countC=0,mem;
-	
+	printf("Input len : ");
 	scanf_s("%d", &len);
+	printf("Input N : ");
 	scanf_s("%d", &N);
 	char c[100][20],res[20];
 	for (int i = 0; i < N; i++)
